05ee5d3021da2b4b759fcab1fc1b2b19130ecf0c
nainajnahO <d.ohanjanian@icloud.com>
2021-10-11 13:33:52 +0200

    11102021

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import math
-
 import pygame
 
 # Initialize pygame ####################################################################################################
@@ -38,16 +36,16 @@ avatar_x = (window_x / 2) - rect_x / 2
 avatar_y = window_y - window_y / 8
 
 # Avatar speed
-avatar_speed = 10
+avatar_speed = 8
 
 # Ball #################################################################################################################
-circle_r = 15
+circle_r = 14
 circle_x = (window_x / 2)
 circle_y = avatar_y - circle_r
 
 
-def draw_ball():
-    pygame.draw.circle(screen, WHITE, (circle_x, circle_y), circle_r, 0)
+def draw_ball(ball_x, ball_y):
+    pygame.draw.circle(screen, WHITE, (ball_x, ball_y), circle_r, 0)
 
 
 # Game engine ##########################################################################################################
@@ -56,7 +54,7 @@ def draw_ball():
 FPS = 60
 
 
-def main(player_x, player_y):
+def main(player_x, player_y, ball_x, ball_y):
     clock = pygame.time.Clock()
     loop = True
     while loop:
@@ -65,19 +63,35 @@ def main(player_x, player_y):
             if event.type == pygame.QUIT:
                 loop = False
 
+        # Rectangle movement
         user_input = pygame.key.get_pressed()
         if user_input[pygame.K_LEFT] and player_x - avatar_speed > 0:
             player_x -= avatar_speed
         if user_input[pygame.K_RIGHT] and player_x + avatar_speed + rect_x < window_x:  # elif
             player_x += avatar_speed
 
+        # Ball boarders & movement
+        # x-movement
+        if ball_x > 0:
+            ball_x -= avatar_speed
+        elif ball_x < window_x:
+            for ball_x in range(window_x):
+                ball_x += avatar_speed
+        # y-movement
+        if ball_y > 0:
+            ball_y -= avatar_speed
+        elif ball_y < window_y:
+            for ball_y in range(window_y):
+                ball_y += avatar_speed
+
+        # Output
         screen.fill(BLACK)
         draw_rect(player_x, player_y, rect_x, rect_y)
-        draw_ball()
+        draw_ball(ball_x, ball_y)
         pygame.display.update()
 
     pygame.quit()
 
 
 if __name__ == '__main__':
-    main(avatar_x, avatar_y)
+    main(avatar_x, avatar_y, circle_x, circle_y)
